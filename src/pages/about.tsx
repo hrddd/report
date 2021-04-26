6ae5cc7870f14381f8f7c78155476759150c72ba
hrddd <nuhhunnuhhun@gmail.com>
2021-04-27 00:49:30 +0900

    add aboutt page

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
