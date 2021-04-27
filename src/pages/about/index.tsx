76ea0736ceb07297104b9422c220f280ea063b4c
hrddd <nuhhunnuhhun@gmail.com>
2021-04-28 00:58:43 +0900

    add css module

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import Header from "../../components/header"
+import Container from '../../components/Container';
+import * as styles from "./index.module.css"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
+export default function About() {
+  return (
+    <Container>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+      <p>Such wow. Very React.</p>
+      <User
+        username="Maria Randall"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-daniel-xavier-1102341.jpg"
+        excerpt="I'm Maria Randall. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Daniela Dewitt"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-guilherme-almeida-1858175.jpg"
+        excerpt="I'm Daniela Dewitt. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </Container>
+  )
+}
